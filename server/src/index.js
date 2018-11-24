cacce3ee084df84285b2a0f0c5a649dba6ecf394
unknown <floriswarmenhoven@gmail.com>
2018-11-24 15:32:13 +0100

    Setup Yoga

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,19 @@
-// let's go!
+require("dotenv").config({ path: "dev.env" });
+const createServer = require("./createServer");
+const db = require("./db");
+
+const server = createServer();
+
+// TODO: Use Express Middleware to handle cookies (JWT) and populate current user
+
+server.start(
+	{
+		cors: {
+			credentials: true,
+			origin: process.env.FRONTEND_URL,
+		},
+	},
+	resp => {
+		console.log(`Server is now live on port http://localhost:${resp.port}`);
+	}
+);
