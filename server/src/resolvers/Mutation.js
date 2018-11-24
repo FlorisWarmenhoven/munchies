270ab668d248b76282137c2f53c4e3ac52c39ed2
unknown <floriswarmenhoven@gmail.com>
2018-11-24 18:13:27 +0100

    Add logic for creating and retrieving items

--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -1,3 +1,10 @@
-const Mutations = {};
+const Mutations = {
+	async createItem(parent, args, { db }, info) {
+		// TODO: Authenticate and authorize user
+		const item = await db.mutation.createItem({ data: { ...args } }, info);
+
+		return item;
+	},
+};
 
 module.exports = Mutations;
--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -1,3 +1,13 @@
-const Query = {};
+const { forwardTo } = require("prisma-binding");
+
+const Query = {
+	items: forwardTo("db"),
+
+	// async items(parent, args, { db }, info) {
+	// 	const items = await db.query.items();
+
+	// 	return items;
+	// },
+};
 
 module.exports = Query;
